0bc36b2825d20a17e9d08a36e658a25bec9ef391
Kota Amemiya <kotaamemiya@gmail.com>
2023-10-04 23:53:37 +0100

    Added median method.

--- median.py
+++ median.py
@@ -1,6 +1,16 @@
 """Median calculator."""
 """ENTER YOUR SOLUTION HERE!"""
 
+def median(numlist):
+    numlist.sort()
+    if len(numlist) % 2 == 0:
+            median = ((numlist[len(numlist)//2])+(numlist[len(numlist)//2-1]))/2
+    else:
+        odd_average = len(numlist)//2
+        median = numlist[odd_average]
+
+    return median
+
 while True:
     try:
         print("Enter a list of numbers separated by commas: ")
@@ -9,4 +19,5 @@ while True:
         print("Some input could not be converted to a number!")
     else:
         break
-print(numbers)
+        
+print(median(numbers))
